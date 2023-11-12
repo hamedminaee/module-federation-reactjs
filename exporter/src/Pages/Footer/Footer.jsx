81a0b586f10b2240619a30be5a02ce62a8c29b2c
Hamed <hamedminaee1984@gmail.com>
2023-11-12 19:26:14 -0400

    test

--- exporter/src/Pages/Footer/Footer.jsx
+++ exporter/src/Pages/Footer/Footer.jsx
@@ -1,33 +1,14 @@
 import React from 'react';
 import './_footer.scss';
 
-function Footer() {
+function Footer({ props }) {
   return (
-    <footer className="footer">
-      <ul className="nav">
-        <li className="nav__item">
-          <a
-            href="https://www.ibm.com/docs/en/environmental-intel-suite?topic=center-data-explorer"
-            className="nav__link"
-            target="_blank"
-            rel="noreferrer"
-          >
-            IBM Project
-          </a>
-        </li>
-        <li className="nav__item">
-          <a
-            href="https://tj.news/telegraph-journal"
-            className="nav__link"
-            target="_blank"
-            rel="noreferrer"
-          >
-            JDI (BNI) project
-          </a>
-        </li>
-      </ul>
-      <p className="copyright">&copy; Copyright 2023 by Hamed Minaee.</p>
-    </footer>
+    <div className="card">
+      <img src={props.avatar} alt="" />
+      <p>
+        {props.first_name}
+      </p>
+    </div>
   );
 }
 
